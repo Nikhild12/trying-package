0932e9fa9224f9d410e59747856aabc0cb77e05f
Nikhid12 <ndhore8@gmail.com>
2021-12-02 00:40:17 +0530

    WIP:created the package

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/**/*.stories.tsx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/react"
+}
